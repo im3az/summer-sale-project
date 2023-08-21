9bc1e564115655e82d05ff10aa6d20f94bb6d2ff
Imtiaz <im3azchowdhury@gmail.com>
2023-08-21 21:12:52 +0600

    added disabled puchase button

--- JS/index.js
+++ JS/index.js
@@ -1,15 +1,26 @@
 let total = 0;
 
-function handleClickDiv(target){
+function handleClickDiv(target) {
+    var purchaseBtn = document.getElementById('purchase-btn')
     const selectedItemContainer = document.getElementById('selected-items');
     const itemName = target.childNodes[3].childNodes[3].innerText;
     const li = document.createElement('li');
     li.innerText = itemName;
     selectedItemContainer.appendChild(li);
     const price = target.childNodes[3].childNodes[5].innerText.split(' ')[0];
-    total = parseInt(total) + parseInt(price);
-    const totalPrice = document.getElementById('total-price').innerText = total;  
-    
-    
+    total = parseFloat(total) + parseFloat(price);
+    const totalPrice = document.getElementById('total-price').innerText = total;
+    if(totalPrice > 0){
+        purchaseBtn.removeAttribute('disabled');
+    }
 }
+
+
+
+// document.getElementById('coupon-value').addEventListener('input',function(){
+//     var totalPrice = document.getElementById('total-price').textContent;
+//     console.log(totalPrice);
+//     var couponBtn = document.getElementById('coupon-btn');
+// })
+
 
