bf82b270a933bc5f8f7ec3374ffce5e109009704
Imtiaz <im3azchowdhury@gmail.com>
2023-08-22 12:01:36 +0600

    added reload button

--- JS/index.js
+++ JS/index.js
@@ -40,8 +40,8 @@ function handleClickDiv(target) {
     })
 }
 
-function goHome(){
-    window.location.href = 'http://127.0.0.1:5500/index.html'
-}
+// function goHome(){
+//     window.location.href = 'http://127.0.0.1:5500/index.html'
+// }
 
 
